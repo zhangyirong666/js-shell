179f05806c1505ed3c0f458db2309fbbfdd35785
zgangyr <2195175822@qq.com>
2019-08-02 16:13:45 +0800

    20190802

--- 二叉树/9.1.js
+++ 二叉树/9.1.js
@@ -14,4 +14,26 @@ function BST(){
 }
 function insert(data){
 	var n = new Node(data, null, null);
+	if(this.root == null){
+		this.root = n;
+	}else{
+		var current = this.root;
+		var parent;
+		while(true){
+			parent = current.left;
+			if(data < current.data){
+				current = current.left;
+				if(current == null){
+					parent.left = n;
+					break;
+				}
+			}else{
+				current = current.right;
+				if(current == null){
+					parent.right = n;
+					break;
+				}
+			}
+		}
+	}
 }
